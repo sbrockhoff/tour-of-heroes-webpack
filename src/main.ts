e861b93c00c75df01fd9bd66b6c565a9b5e726b3
Shaun Brockhoff <Shaun Brockhoff>
2016-08-13 22:04:43 -0500

    Update configuration to use HashLocationStrategy for S3 compatibility

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 import {XHRBackend} from '@angular/http';
 import {InMemoryBackendService, SEED_DATA} from 'angular2-in-memory-web-api';
 import {InMemoryDataService} from './app/in-memory-data.service';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 
 import {bootstrap}    from '@angular/platform-browser-dynamic';
 import {HTTP_PROVIDERS} from '@angular/http';
@@ -12,6 +13,7 @@ import {appRouterProviders} from './app/app.routes';
 bootstrap(AppComponent, [
   appRouterProviders,
   HTTP_PROVIDERS,
+  {provide: LocationStrategy, useClass: HashLocationStrategy},
   {provide: XHRBackend, useClass: InMemoryBackendService},  //in-mem server
   {provide: SEED_DATA, useClass: InMemoryDataService} //in-mem server data
 ]);
